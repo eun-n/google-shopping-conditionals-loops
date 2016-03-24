16905c811631f8cffbbdf88623419fc8bf74ea91
eun-n <eunicena.12@gmail.com>
2016-03-23 21:48:31 -0700

    2 works

--- google_shopping.js
+++ google_shopping.js
@@ -10,9 +10,9 @@ var data = require("./products.json")
 // } console.log(count);
 // 25 items with shopping#product in "kind"
 
-//2. *not working*
+//2.
 // for (var i = 0; i < data["items"].length; i++) {
-// 	if (data["items"][i].product.inventories) {
+// 	if (data.items[i].product.inventories[0].availability == "backorder") {
 // 		console.log(data["items"][i]);
 // 	}
 // }
@@ -20,29 +20,29 @@ var data = require("./products.json")
 
 //3. 
 // for (var i = 0; i < data["items"].length; i++) {
-// 	if (data["items"][i].product.images.length >1) {
+// 	if (data.items.[i].product.images.length >1) {
 // 		console.log(data["items"][i]);
 // 	}
 // }
 
 //4. 
 // for (var i = 0; i < data["items"].length; i++) {
-// 	if (data["items"][i].product.brand == "Canon") {
+// 	if (data.items.[i].product.brand == "Canon") {
 // 		console.log(data["items"][i]);
 // 	}
 // }
 
 //5. 
 // for (var i = 0; i < data["items"].length; i++) {
-// 	if (data["items"][i].product.author.name == "eBay" && data["items"][i].product.brand == "Canon") {
+// 	if (data.items.[i].product.author.name == "eBay" && data.items.[i].product.brand == "Canon") {
 // 		console.log(data["items"][i]);
 // 	}
 // }
 
 // 6. *not working*
 // for (var i = 0; i < data["items"].length; i++) {
-// 	if (data["items"][i].product.images.length >1) {
-// 		console.log(data["items"][i].brand + data["items"][i].inventories["price"] + data["items"][i].product.images[1]);
+// 	if (data.items.[i].product.images.length >1) {
+// 		console.log(data.items.[i].brand + data.items.[i].inventories.price + data.items.[i].product.images[1]);
 // 	}
 // }
 // console.log(data["items"]);
