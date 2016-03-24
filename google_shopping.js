fcf859761824c115f29adc1f2a95acd0559b33b6
eun-n <eunicena.12@gmail.com>
2016-03-23 21:39:48 -0700

    2&6 not solved

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,48 @@
 var data = require("./products.json")
 
 // Write your solutions below
+// 1. 
+// var count = 0
+// for (var i = 0; i < data["items"].length; i++) {
+// 	if (data["items"][i].kind === "shopping#product") {
+// 		count++;
+// 	}
+// } console.log(count);
+// 25 items with shopping#product in "kind"
 
-console.log(data["items"]);
+//2. *not working*
+// for (var i = 0; i < data["items"].length; i++) {
+// 	if (data["items"][i].product.inventories) {
+// 		console.log(data["items"][i]);
+// 	}
+// }
+
+
+//3. 
+// for (var i = 0; i < data["items"].length; i++) {
+// 	if (data["items"][i].product.images.length >1) {
+// 		console.log(data["items"][i]);
+// 	}
+// }
+
+//4. 
+// for (var i = 0; i < data["items"].length; i++) {
+// 	if (data["items"][i].product.brand == "Canon") {
+// 		console.log(data["items"][i]);
+// 	}
+// }
+
+//5. 
+// for (var i = 0; i < data["items"].length; i++) {
+// 	if (data["items"][i].product.author.name == "eBay" && data["items"][i].product.brand == "Canon") {
+// 		console.log(data["items"][i]);
+// 	}
+// }
+
+// 6. *not working*
+// for (var i = 0; i < data["items"].length; i++) {
+// 	if (data["items"][i].product.images.length >1) {
+// 		console.log(data["items"][i].brand + data["items"][i].inventories["price"] + data["items"][i].product.images[1]);
+// 	}
+// }
+// console.log(data["items"]);
